a2ae1325fac6707d321785d4768d5c7c155efac8
Adam Keino <keinoadamdev@gmail.com>
2022-03-16 07:02:40 +0000

    Debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ * main - tests function that prints either positive or negative.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
