69ba5da61f2d76964150a46d63a16b4f97715006
Adam Keino <keinoadam@gmail.com>
2022-03-21 19:07:44 +0000

    Keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates a key
+ * Return: 0
+ */
+
+int main(void)
+{
+	int m = 0, n = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (n < 2772)
+	{
+		r = rand() % 128;
+		if ((n + m) > 2772)
+			break;
+		n = n + m;
+		printf("%c", m);
+	}
+	printf("%c\n", (2772 - n));
+	return (0);
+}
