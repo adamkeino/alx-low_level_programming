0c9b019733ce5f9fc9a51ed27566324bca4c8d61
Adam Keino <keinoadam@gmail.com>
2022-04-01 12:39:25 +0000

    unused

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 /**
  * main - prints argv[0]
- * @agrc: argument counter
- * @argv[]: array of arguments
+ * @argc: argument counter
+ * @argv: array of arguments
  * Return: 0
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%s", argv[0]);
+	(void)argc;
+	printf("%s", argv[0]);
 	return (0);
 }
