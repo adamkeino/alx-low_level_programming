ad5152696665236c33273f1b488d992dc4860686
Adam Keino <keinoadam@gmail.com>
2022-04-01 12:01:32 +0000

    Slimshady

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints argv[0]
+ * @agrc: argument counter
+ * @argv[]: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	printf ("%s", argv[0]);
+	return (0);
+}
