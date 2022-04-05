ddaf5aa7f1d44258782517085247d1f806609c83
Adam Keino <keinoadam@gmail.com>
2022-04-05 12:00:31 +0000

    Move

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,16 +12,16 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i, j, c, p, size;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		;
 
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
 
 	size = i + j;
 	s3 = (char *)malloc(sizeof(char) * (size + 1));
