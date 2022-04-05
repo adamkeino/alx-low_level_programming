a88ec723f9c5ad34bc193c5b3992abe49847a19c
Adam Keino <keinoadam@gmail.com>
2022-04-05 10:54:44 +0000

    Why?

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,33 +1,29 @@
-#include <string.h>
-#include "main.h"
+#include "holberton.h"
 #include <stdlib.h>
 /**
- * _strdup - points to a duplicate string
- * @str: the string to compare
- * Return: pointer to the duplicate or NULL
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ *
+ * Return: pointer of an array of chars
  */
-
 char *_strdup(char *str)
 {
-	unsigned int size = strlen(str);
-	char *m = (char *)malloc((size * sizeof(char) + 1));
-	unsigned int i;
-
+	char *strout;
+	unsigned int i, j;
 
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-	else if (m == NULL)
-	{
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strout == NULL)
 		return (NULL);
-	}
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			m[i] = str[i];
-		}
-		return (m);
-	}
+
+	for (j = 0; j <= i; j++)
+		strout[j] = str[j];
+
+	return (strout);
 }
