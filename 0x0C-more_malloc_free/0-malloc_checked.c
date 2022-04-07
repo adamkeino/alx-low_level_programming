4f57f30dc9d513c5132cd14641575fa27d06e655
Adam Keino <keinoadam@gmail.com>
2022-04-07 09:30:44 +0000

    This could be

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,6 +4,7 @@
  * malloc_checked - allocates memory
  * @b: size in bytes
  * Return: pointer to the memory created.
+ * if malloc fails, exit with status 98
  */
 
 void *malloc_checked(unsigned int b)
