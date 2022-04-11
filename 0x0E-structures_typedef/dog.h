a45f09ce0a752f640e4c152fbf496f0d4a4cfede
Adam Keino <keinoadam@gmail.com>
2022-04-11 09:12:08 +0000

    Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - defines a dog by name, age, owner
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: name age and owner used to describe dog
+ */
 struct dog
 {
 	char *name;
