e67b3a7a25849224e2d0f451da9b5bfd80f58292
Adam Keino <keinoadam@gmail.com>
2022-04-13 11:31:23 +0000

    T0

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - iterates through an array
+ * performing functions that have been pointed to
+ * @array: the array
+ * @size: the array size
+ * @action: pointer to the function
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+}
