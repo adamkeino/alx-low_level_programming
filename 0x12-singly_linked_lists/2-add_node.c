7f68163d142780b1d742b6976fc573c1da406f04
Adam Keino <keinoadam@gmail.com>
2022-04-21 14:17:52 +0000

    Adding a node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,8 +10,17 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *newNode;
 
+	if (str == NULL)
+		return (NULL);
 	newNode = (list_t *)malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
 	newNode->str = strdup(str);
+	if (newNode->str == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
 	newNode->len = strlen(str);
 	newNode->next = *head;
 	*head = newNode;
