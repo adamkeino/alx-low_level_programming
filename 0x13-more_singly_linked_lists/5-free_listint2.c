5eaea22792bb19e7dd43c7dc6c2948150023a851
Adam Keino <keinoadam@gmail.com>
2022-04-26 08:42:28 +0000

    return

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current, *next;
 
+	if (*head == NULL)
+		return;
 	current = *head;
 	while (current != NULL)
 	{
@@ -15,5 +17,4 @@ void free_listint2(listint_t **head)
 		free(current);
 		current = next;
 	}
-	*head = NULL;
 }
