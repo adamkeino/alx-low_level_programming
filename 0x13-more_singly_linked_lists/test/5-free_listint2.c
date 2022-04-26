80d7c8d523c5a4a2861c635cc0ffe80b6d9e5805
Adam Keino <keinoadam@gmail.com>
2022-04-26 08:28:48 +0000

    ??

---
+++ 0x13-more_singly_linked_lists/test/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees list
+ * @head: pointer to pointer to start of list
+ * return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current, *next;
+
+	current = *head;
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
