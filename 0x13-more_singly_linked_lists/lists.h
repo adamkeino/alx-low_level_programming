8c784fbc6297b41aa033ce150f0abd0c68cc3da4
Adam Keino <keinoadam@gmail.com>
2022-04-26 05:48:09 +0000

    end node

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -42,5 +42,6 @@ void before_main(void) __attribute__((constructor));
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif
