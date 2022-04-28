e39d2e5ec193c5d114110d0f6dd4eaee991c3c51
Adam Keino <keinoadam@gmail.com>
2022-04-28 13:44:07 +0000

    dec to bin

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,5 +8,6 @@
 char _putchar(char *);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
+unsigned long int _pow(unsigned int base, unsigned int power);
 
 #endif
