9800067347238deb1f62371b49d00f973499c898
Adam Keino <keinoadam@gmail.com>
2022-04-28 13:54:52 +0000

    Get bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ int  _putchar(char);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int base, unsigned int power);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif
