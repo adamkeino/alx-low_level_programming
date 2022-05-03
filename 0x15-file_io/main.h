dc6578d317d4dd6ec12ee6412abd5b425228b47d
Adam Keino <keinoadam@gmail.com>
2022-05-03 15:06:01 +0000

    Fuches

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,4 +7,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 size_t _strlen(char *);
 int append_text_to_file(const char *filename, char *text_content);
+void error_file(int file_from, int file_to, char *argv[]);
+int main(int argc, char *argv[]);
 #endif
