d42e80b16e2fe426ccaf79a94e7dd51f810e0c4d
Adam Keino <keinoadam@gmail.com>
2022-05-03 14:28:13 +0000

    Sucks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 #include <stdio.h>
+
 int _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+size_t _strlen(char *);
 
 #endif
--- 0x15-file_io/test/main.h
+++ 0x15-file_io/test/main.h
@@ -4,5 +4,7 @@
 
 int _putchar(void);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+size_t _strlen(char *);
 
 #endif
