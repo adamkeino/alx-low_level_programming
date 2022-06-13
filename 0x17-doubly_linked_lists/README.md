1beb9c9c161f42e477a6ff500c6b051086edbf7d
Adam Keino <keinoadam@gmail.com>
2022-06-13 12:02:54 +0000

    hello

---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,4 @@
+# Doubly linked lists
+In this section, we study doubly linked lists. Objectives include:
+- What is a doubly linked list
+- How to use doubly linked lists
---
+++ 0x17-doubly_linked_lists/test/0-print_dlistint.c
@@ -0,0 +1,8 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all elements of a list
+ * @h - pointer to the list
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+
