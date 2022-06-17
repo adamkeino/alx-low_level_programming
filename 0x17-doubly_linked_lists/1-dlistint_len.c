21da428aa89a04ba963a66750df9c165acde487d
Adam Keino <keinoadam@gmail.com>
2022-06-17 09:18:50 +0000

    betty

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,7 +3,7 @@
 /**
  * dlistint_len - finds number of elements in a linked list
  * @h: pointer to the list
- * @Return: number of elements in the list
+ * Return: number of elements in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
