76c36aeef35b6ad4a73c7a11535146304ef8d85c
Adam Keino <keinoadam@gmail.com>
2022-06-17 09:16:17 +0000

    elem

---
+++ 0x17-doubly_linked_lists/test/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - finds number of elements in a linked list
+ * @h: pointer to the list
+ * @Return: number of elements in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t ans;
+
+	ans = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		ans++;
+	}
+	return (ans);
+}
